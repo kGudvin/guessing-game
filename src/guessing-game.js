8835558b7799a6dc189a6cdeba21b6e0e815c22d
Kirill Gudeta <kirillgudeta@gmail.com>
2024-09-13 01:39:49 +0300

    easy rly

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,27 @@
 class GuessingGame {
-    constructor() {}
+	constructor(min, max, current_guess) {
+		this.min = 0
+		this.max = 0
+		this.current_guess = 0
+	}
 
-    setRange(min, max) {
+	setRange(min, max) {
+		this.min = min
+		this.max = max
+	}
 
-    }
+	guess() {
+		this.current_guess = Math.round((this.min + this.max) / 2)
+		return this.current_guess
+	}
 
-    guess() {
+	lower() {
+		this.max = this.current_guess
+	}
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+	greater() {
+		this.min = this.current_guess
+	}
 }
 
-module.exports = GuessingGame;
+module.exports = GuessingGame
